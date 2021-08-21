d3543ea2aef4d392465d7bf94264e7ad53aa0e4e
artn-dev <azemarteixeira@gmail.com>
2021-08-21 02:35:29 -0300

    Diretorio destino para compilaçao

--- Makefile
+++ Makefile
@@ -1,4 +1,13 @@
 .SUFFIXES: .v .tb.v .vcd
 VPATH = nivel3
 
+OUT := $(CURDIR)/build
+
 include */*.mk
+
+$(OUT)/:
+	mkdir -p $@
+
+.PHONY: clean
+clean: $(OUT)/
+	rm -rf $(OUT)
--- nivel3/level3.mk
+++ nivel3/level3.mk
@@ -2,11 +2,8 @@ objs := BCDCounter_mod6.vcd BCDCounter_mod10.vcd
 
 all: $(objs)
 
-$(objs): %.vcd: %.v %.tb.v
+$(objs): %.vcd: %.v %.tb.v $(OUT)/
 	iverilog $^ -o foo
 	vvp foo
 	rm foo
-
-.PHONY: clean
-clean:
-	rm $(objs)
+	mv $(@D)/$(@F) $(OUT)/$(@F)
