558840cc182315bc5f3753b871a0161a24d5e9ce
artn-dev <azemarteixeira@gmail.com>
2021-08-22 18:56:49 -0300

    contador 0-7

---
+++ nivel3/Counter7.v
@@ -0,0 +1,18 @@
+module Counter7(
+    input  wire clk, clrn,
+    output reg  out
+);
+    reg [2:0] state;
+
+    always @(posedge clk, negedge clrn) begin
+        if (~clrn)
+            state = 3'd0;
+        else if (state < 3'd7)
+            state = state + 3'd1;
+    end
+
+    always @(state) begin
+        out = state >= 3'd4;
+    end
+
+endmodule
