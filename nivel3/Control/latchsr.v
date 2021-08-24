7edc52ae402f1a9c27e06198877b50449eff5c45
artn-dev <azemarteixeira@gmail.com>
2021-08-24 11:47:41 -0300

    simplify logic

--- nivel3/Control/latchsr.v
+++ nivel3/Control/latchsr.v
@@ -1,31 +1,8 @@
 module latchsr(
     input wire r, s,
-    output wire q
+    output wire q, qn
 );
+    assign q  = r ~| qn;
+    assign qn = s ~| q;
 
-    reg q2 = 0;
-    assign q = q2;
-    
-    always @(r, s) begin
-        
-        if(r == 0 & s == 0)
-        begin
-            q2 = q2;
-        end
-        
-        else if(r == 1 & s == 0)
-        begin
-            q2 = 0;
-        end
-        
-        else if(r == 0 & s == 1)
-        begin
-            q2 = 1;
-        
-        end
-    end
 endmodule
-
-
-
-
