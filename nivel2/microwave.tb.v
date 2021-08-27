21d0ffcc677ae3aa9dad4299d957cf9ee55493e3
artn-dev <azemarteixeira@gmail.com>
2021-08-27 02:02:14 -0300

    release start button

--- nivel2/microwave.tb.v
+++ nivel2/microwave.tb.v
@@ -30,7 +30,8 @@ module microwave_tb;
         start=1; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #1000;  // soltar 1
         start=1; stop=1; clear=1; door_closed=1; kbd=10'b0000000100; #1000;  // pressionar 2
         start=1; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #1000;  // soltar 2
-        start=0; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #14000; // pressionar start
+        start=0; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #1000;  // pressionar start
+        start=1; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #14000; // soltar start
 
         // repouso
         start=1; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #1000;
@@ -40,7 +41,8 @@ module microwave_tb;
         start=1; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #1000; // soltar 3
         start=1; stop=1; clear=1; door_closed=1; kbd=10'b0000100000; #1000; // pressionar 5
         start=1; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #1000; // soltar 5
-        start=0; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #5000; // pressionar start
+        start=0; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #1000; // pressionar start
+        start=1; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #5000; // soltar start
 
         // stop
         start=1; stop=0; clear=1; door_closed=1; kbd=10'b0000000000; #1000; // pressionar stop
@@ -52,7 +54,8 @@ module microwave_tb;
         start=1; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #1000; // soltar 2
         start=1; stop=1; clear=1; door_closed=1; kbd=10'b1000000000; #1000; // pressionar 9
         start=1; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #1000; // soltar 9
-        start=0; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #3000; // pressionar start
+        start=0; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #1000; // pressionar start
+        start=1; stop=1; clear=1; door_closed=1; kbd=10'b0000000000; #3000; // soltar start
 
         // abrir porta
         start=1; stop=1; clear=1; door_closed=0; kbd=10'b0000000000; #1000;
