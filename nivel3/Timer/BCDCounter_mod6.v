3d0364da67b3bd1ee5de2909c9e8ee6c4c3bd27b
artn-dev <azemarteixeira@gmail.com>
2021-08-27 12:43:16 -0300

    refactor logic

--- nivel3/Timer/BCDCounter_mod6.v
+++ nivel3/Timer/BCDCounter_mod6.v
@@ -4,31 +4,22 @@ module BCDCounter_mod6 (
   output reg  [3:0] out,
   output reg  tc, zero
 );
-  reg [3:0] curr_state, next_state;
+  reg [3:0] curr_state;
 
   always @(posedge clk, negedge loadn, negedge clrn) begin
-    if (~clrn)
-      curr_state <= 4'd0;
-    else if (~loadn)
+    if (~clrn) begin
+      curr_state <= 0;
+    end
+    else if (~loadn) begin
       curr_state <= data;
-    else if (en)
-      curr_state <= next_state;
-  end
+    end
+    else if (en) begin
+      tc <= (curr_state == 0) ? 1 : 0;
+      curr_state <= (curr_state == 0) ? 5 : curr_state - 1;
+    end
 
-  always @(curr_state) begin
+    zero = (curr_state == 0) ? 1 : 0;
     out = curr_state;
-    tc = 1'b0;
-
-    if (curr_state == 4'd0) begin
-      next_state = 4'd5;
-      zero = 1'b1;
-      if (en)
-        tc = 1'b1;
-    end
-    else begin
-      next_state = curr_state - 4'd1;
-      zero = 1'b0;
-    end
   end
 
 endmodule
